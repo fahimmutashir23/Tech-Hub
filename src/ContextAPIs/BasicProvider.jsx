13d32157f70cfafda81d954a4cc729915772d9c1
MD FAHIM MUNTASHIR <taslimit23@gmail.com>
2024-06-11 09:22:01 +0600

    hi

--- src/ContextAPIs/BasicProvider.jsx
+++ src/ContextAPIs/BasicProvider.jsx
@@ -1,10 +1,20 @@
 /* eslint-disable react/prop-types */
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import useSmallScreen from "../Hooks/useSmallScreen";
 
 export const BasicContext = createContext(null);
 
 const BasicProvider = ({ children }) => {
   const [sidebarOpen, setSideBarOpen] = useState(true);
+  const [isSmallScreen] = useSmallScreen()
+
+  useEffect(() => {
+    if(isSmallScreen){
+      setSideBarOpen(true)
+    } else{
+      setSideBarOpen(false)
+    }
+  }, [])
 
   const info = {
     sidebarOpen,
